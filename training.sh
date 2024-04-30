d5defd46930ed5236da74a3deef3c4e33e12d0eb
Huandong <huandongchang@hotmail.com>
2024-04-30 19:57:04 -0400

    Training Complete

--- training.sh
+++ training.sh
@@ -1,11 +1,13 @@
 cd yolov9
 python train.py \
---batch 16 --epochs 300 --img 640 --device 0 --min-items 0 --close-mosaic 200 \
+--batch 16 --epochs 300 --img 640 --device 0 --min-items 0 --close-mosaic 40 \
 --data ../data33.yaml \
 --weights ../weights/gelan-c.pt \
 --cfg models/detect/gelan-c.yaml \
 --hyp hyp.scratch-high.yaml \
 --optimizer AdamW \
---patience 50
+--workers 4 \
+--patience 100 \
+--resume
 
 cd ..
