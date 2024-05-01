93b796cfbf61ed6846350b2050e87cbd1bf351db
Huandong <huandongchang@hotmail.com>
2024-04-30 20:00:46 -0400

    Test Yolo on Videos

---
+++ video-testing.sh
@@ -0,0 +1,5 @@
+python /home/huandong/CV/yolov9/detect.py \
+--weights /home/huandong/CV/yolov9/runs/train/exp7/weights/best.pt \
+--source /home/huandong/CV/Cambridge.mov \
+--name video_clean_300eph \
+--device 0
